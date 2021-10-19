6aa23dd141a9db12695ec5be95d8cf60089be55b
regtm <6105545+regtm@user.noreply.github.com>
2021-10-19 15:20:21 +0200

    Add tap arrow keys on ctrl, shift, super, fn

--- interphase/keymaps/classic/keymap.c
+++ interphase/keymaps/classic/keymap.c
@@ -1,4 +1,4 @@
-#include "interphase.h"
+ #include "interphase.h"
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -25,17 +25,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	{BTAB,      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC },
 	{KC_CAPS,   KC_NUBS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_BSLS },
 	{KC_LSFT,   XXXXXXX,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,    KC_RSFT },
-	{KC_LCTL,   KC_LGUI,   KC_LALT, FNKEY,   KC_BSPC, KC_SPC,  XXXXXXX,   XXXXXXX, KC_SPC,  KC_ENT,  FNKEY,   KC_RALT, KC_RGUI,    KC_RCTL }
+	{KC_LCTL,   KC_LGUI,   FNKEY,   KC_LALT, KC_BSPC, KC_SPC,  XXXXXXX,   XXXXXXX, KC_SPC,  KC_ENT,  KC_RALT, FNKEY,   KC_RGUI,    KC_RCTL }
 },
 
 
 [_FUNCTION] = { /* Function Layer, primary alternative layer featuring numpad on left hand,
                                    cursor keys on right hand*/
 	{KC_ESC,  XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12},
- 	{KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX,   KC_PGUP, KC_END,  KC_UP,   KC_HOME, XXXXXXX, XXXXXXX, XXXXXXX },
+ 	{KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX,   KC_PGUP, KC_HOME, KC_UP,  KC_END,   XXXXXXX, XXXXXXX, XXXXXXX },
  	{XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX,   KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX },
  	{XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX },
- 	{XXXXXXX, XXXXXXX, XXXXXXX, FNKEY,   KC_BSPC, KC_SPC,  XXXXXXX,   XXXXXXX, KC_SPC,  KC_ENT,  FNKEY,   XXXXXXX, XXXXXXX, XXXXXXX }
+ 	{KC_LCTL, KC_LGUI, FNKEY,   KC_LALT, KC_BSPC, KC_SPC,  XXXXXXX,   XXXXXXX, KC_SPC,  KC_ENT,  KC_RALT, FNKEY,   KC_RGUI, KC_RCTL }
 },
 
 };
